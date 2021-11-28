8db2680ad85b26c889ec88b21fa97a706e569c5d
Jpedro0611 <92061184+Jpedro0611@users.noreply.github.com>
2021-11-28 11:31:11 -0300

    Add files via upload
    colocar as bibliotecas em uma pasta nova com o nome que esta no HTML assim com as classes e as imagens

---
+++ sling.js
@@ -0,0 +1,35 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.Sling = Constraint.create(options);
+        World.add(world, this.Sling);
+    }
+    fly () {
+        this.Sling.bodyA = null
+
+    }
+    back (body) {
+        this.Sling.bodyA = body;
+
+
+    }
+    display(){
+        
+
+        if (this.Sling.bodyA){
+            var pointA = this.Sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+        }
+    }
+    
+}
